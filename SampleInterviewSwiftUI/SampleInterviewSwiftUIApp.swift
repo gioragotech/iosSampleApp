e2c6c60a9595298d1f5d9faab0d4259044fe8e10
giora <giorak@mixin.co.il>
2025-06-06 08:54:22 +0300

    Initial Commit

---
+++ SampleInterviewSwiftUI/SampleInterviewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleInterviewSwiftUIApp.swift
+//  SampleInterviewSwiftUI
+//
+//  Created by giora krasilshchik on 06/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct SampleInterviewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
